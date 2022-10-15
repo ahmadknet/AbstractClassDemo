2a71efb3ff9585f942a33527441b266b7c424039
ahmadk.net@gmail.com <ahmadk.net@gmail.com>
2022-10-15 18:18:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace AbstractClass
+{    
+    abstract class Language
+    {
+        public abstract void display1();
+        // non-abstract method
+        public void display()
+        {
+            Console.WriteLine("Non abstract method");
+        }
+    }
+
+    // inheriting from abstract class
+    class Program : Language
+    {
+        public override void display1()
+        {
+            Console.WriteLine("Nsdson abstract method");
+        }
+        static void Main(string[] args)
+        {
+            
+            // object of Program class
+            Program obj = new Program();
+
+            // access method of an abstract class
+            obj.display1();
+
+            Console.ReadLine();
+        }
+    }
+}
